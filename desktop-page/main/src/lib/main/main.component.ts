340fe797dc9509fc94c6555e5ff4985cf2c4baa8
Иван Солдатов <ivansoldatov@MacBook-Pro-Ivan.local>
2024-08-01 18:33:36 +0300

    add

--- desktop-page/main/src/lib/main/main.component.ts
+++ desktop-page/main/src/lib/main/main.component.ts
@@ -121,7 +121,7 @@ export class MainComponent {
       text: 'Идея',
     },
     {
-      icon: 'tuiIconAwardLarge',
+      icon: 'tuiIconShoppingCartLarge',
       text: 'Сдекла',
     },
   ];
@@ -132,12 +132,12 @@ export class MainComponent {
       text: 'График',
     },
     {
-      icon: 'tuiIconLogInLarge',
-      text: 'Вход',
+      icon: 'tuiIconTargetLarge',
+      text: 'Идея',
     },
     {
-      icon: 'tuiIconLogOutLarge',
-      text: 'Выход',
+      icon: 'tuiIconShoppingCartLarge',
+      text: 'Сдекла',
     },
   ];
 
