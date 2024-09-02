fbcb2d904de5dd0e72b84080201e2cbc5df0f308
Иван Солдатов <ivansoldatov@MacBook-Pro-Ivan.local>
2024-09-02 16:02:26 +0300

    add atr button

--- desktop-page/main/src/lib/main/chart/chart.component.ts
+++ desktop-page/main/src/lib/main/chart/chart.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 import { AsyncPipe, NgIf } from '@angular/common';
 import { ChartComponent } from '@ui/chart';
-import { combineLatest, debounceTime, Observable, shareReplay } from 'rxjs';
+import { BehaviorSubject, combineLatest, debounceTime, Observable, shareReplay, Subject, switchMap } from 'rxjs';
 import { StockInstrument } from 'types/stock';
 import { DesktopLkStore } from 'stores/desktop';
 import { DESKTOP_STORE } from 'tokens/desktop';
@@ -9,7 +9,7 @@ import { ButtonWithListComponent } from './button-with-list/button-with-list.com
 import { CHART_EMA_LIST, CHART_SMA_LIST } from './chart.constants';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { sortText } from 'utils/sort-text';
-import { TuiLoaderModule } from '@taiga-ui/core';
+import { TuiButtonModule, TuiLoaderModule, TuiSvgModule } from '@taiga-ui/core';
 import { map } from 'rxjs/operators';
 
 interface IndicatorListItem {
@@ -20,13 +20,24 @@ interface IndicatorListItem {
 @Component({
   selector: 'lib-chart',
   standalone: true,
-  imports: [AsyncPipe, ChartComponent, ButtonWithListComponent, ReactiveFormsModule, TuiLoaderModule, NgIf],
+  imports: [
+    AsyncPipe,
+    ChartComponent,
+    ButtonWithListComponent,
+    ReactiveFormsModule,
+    TuiLoaderModule,
+    NgIf,
+    TuiButtonModule,
+    TuiSvgModule,
+  ],
   templateUrl: './chart.component.html',
   styleUrl: './chart.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MainChartComponent implements OnInit {
   private readonly _store: DesktopLkStore = inject(DESKTOP_STORE);
+  toggle = true;
+  private readonly _text$: Subject<boolean> = new BehaviorSubject<boolean>(this.toggle);
   readonly selected$: Observable<StockInstrument | null> = this._store.selectedInstrument$;
   readonly candles$: Observable<any | null> = this._store.candles$.pipe(shareReplay(1));
   readonly indicators$: Observable<any[]> = combineLatest([this._store.indicatorEma$, this._store.indicatorSma$]).pipe(
@@ -35,14 +46,13 @@ export class MainChartComponent implements OnInit {
   );
   readonly consolidationZones$: Observable<any | null> = this._store.consolidationZones$;
   readonly text$: Observable<string | null> = this._store.indicatorAtr$.pipe(
-    map((value: { atr: number }) => value && `1 ATR: ${value.atr}`)
+    map((value: { atr: number }) => value && `1 ATR: ${value.atr}`),
+    switchMap((text: string | null) => this._text$.asObservable().pipe(map((value: boolean) => (value ? text : null))))
   );
 
   emaList: IndicatorListItem[] = CHART_EMA_LIST;
-  isOpenEma = false;
   valueEma: IndicatorListItem[] | null = null;
   smaList: IndicatorListItem[] = CHART_SMA_LIST;
-  isOpenSma = false;
   valueSma: IndicatorListItem[] | null = null;
 
   readonly controlEma: FormControl<IndicatorListItem[] | null> = new FormControl([this.emaList[1], this.emaList[4]]);
@@ -58,21 +68,34 @@ export class MainChartComponent implements OnInit {
     }
   }
 
-  onendedEma(event: boolean): void {
-    this.isOpenEma = event;
+  onToggle(event: Event): void {
+    event.preventDefault();
 
+    this.toggle = !this.toggle;
+    this._text$.next(this.toggle);
+  }
+
+  onOpenedEma(event: boolean): void {
     if (!event && this.valueEma !== this.controlEma.value) {
-      this.valueEma = this.controlEma.value;
-      this._store.updateIndicatorEmaSelected(this._getValue(this.controlEma.value));
+      this._actionEma();
     }
   }
 
-  onendedSma(event: boolean): void {
-    this.isOpenSma = event;
+  onToggledEma(): void {
+    if (this.valueEma !== this.controlEma.value) {
+      this._actionEma();
+    }
+  }
 
+  onOpenedSma(event: boolean): void {
     if (!event && this.valueSma !== this.controlSma.value) {
-      this.valueSma = this.controlSma.value;
-      this._store.updateIndicatorSmaSelected(this._getValue(this.controlSma.value));
+      this._actionSma();
+    }
+  }
+
+  onToggledSma(): void {
+    if (this.valueSma !== this.controlSma.value) {
+      this._actionSma();
     }
   }
 
@@ -82,5 +105,15 @@ export class MainChartComponent implements OnInit {
     }
 
     return value.map((item: IndicatorListItem) => item.value).sort(sortText);
+  }
+
+  private _actionEma(): void {
+    this.valueEma = this.controlEma.value;
+    this._store.updateIndicatorEmaSelected(this._getValue(this.controlEma.value));
+  }
+
+  private _actionSma(): void {
+    this.valueSma = this.controlSma.value;
+    this._store.updateIndicatorSmaSelected(this._getValue(this.controlSma.value));
   }
 }
